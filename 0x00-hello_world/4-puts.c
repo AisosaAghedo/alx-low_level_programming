8256e7eb26132fc8df09adfab959eb94a36b6a91
Aisosa <allysonaghedo@gmail.com>
2022-04-07 12:01:36 -0700

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main(void)
 {
-	char cProgram[] = "programming is like building a multilingual puzzle\n";
+	char cProgram[] = 
+		"\"programming is like building a multilingual puzzle";
 	puts(cProgram);
 	return (0);
 }
