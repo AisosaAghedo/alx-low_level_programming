6891df240926ea3f9f599cff1bcbf12399b48494
Aisosa <allysonaghedo@gmail.com>
2022-04-12 15:46:01 -0700

    last digit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,7 +6,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _putchar(char n);
 int _abs(int n);
-int _print_last_digit(int n);
+int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
 int add(int,int);
