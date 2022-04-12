b242343417ae14cf42703930205f039c06d732a4
Aisosa <allysonaghedo@gmail.com>
2022-04-12 16:45:01 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,11 +10,12 @@ void times_table(void)
 	int x;
 	int y;
 	int z;
+
 	for (x = 0; x <= 9; x++)
 	{
 		for (y = 0; y <= 9; y++)
 		{
-			z = x*y;
+			z = x * y;
 			if ((z / 10) == 0)
 			{
 				if (y == 0)
