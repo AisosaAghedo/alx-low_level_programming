91f356e05b38f67425cf8340154ee3b642d4edf1
Aisosa <allysonaghedo@gmail.com>
2022-04-12 16:57:36 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * add - Entry point
+ *
+ * Return: Always 0
+ */
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b
+		printf("%d\n", n);
+	return (0);
+}
