8490a80fc4d915af820fde2385822837f9b1f7ef
Aisosa <allysonaghedo@gmail.com>
2022-04-12 11:25:23 -0700

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * _islower - Entry point
+ *
+ * Return 0
+ */
+int _islower(int c)
+{
+	int num;
+	
+
+	if ('c' == 'c')
+	{
+		num = 1;
+	}
+	else
+	{
+		num = 0;
+	}
+	return num;
+}
