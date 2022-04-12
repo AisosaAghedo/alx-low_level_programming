607102422153de1eccc30f903ecda9421a529ebe
Aisosa <allysonaghedo@gmail.com>
2022-04-12 16:39:17 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,19 +14,19 @@ void times_table(void)
 	{
 		for (y = 0; y <= 9; y++)
 		{
-			c = a*b;
-			if ((c / 10) == 0)
+			z = a*b;
+			if ((z / 10) == 0)
 			{
-				if (b == 0)
+				if (y == 0)
 				{
 					_putchar('0');
 				}
-				if (b != 0)
+				if (y != 0)
 				{
 					_putchar(' ');
-					_putchar((c % 10) + '0');
+					_putchar((z % 10) + '0');
 				}
-				if (b < 9)
+				if (y < 9)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -34,9 +34,9 @@ void times_table(void)
 			}
 			else
 			{
-				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
-				if (b < 9)
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+				if (y < 9)
 				{
 					_putchar(',');
 					_putchar(' ');
