6bf56e9dad280a715d5d38f96ca640963eb9ec99
Aisosa <allysonaghedo@gmail.com>
2022-04-12 16:04:30 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,7 +6,11 @@
  */
 void jack_bauer(void)
 {
+	int a;
+	int b;
+
 	for (a = 0; a <= 23; a++)
+	{
 	for (b = 0; b <= 59; b++)
 	{
 		_putchar(a / 10 + '0');
@@ -15,5 +19,6 @@ void jack_bauer(void)
 		_putchar(b / 10 + '0');
 		_putchar(b % 10 + '0');
 		_putchar('\n');
+	}
 	}
 }
