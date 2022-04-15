1b8466aa7fd90e14f85e731b054c09f67a534bf6
Aisosa <allysonaghedo@gmail.com>
2022-04-14 17:04:18 -0700

    upper case

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 #include <ctype.h>
-/** 
- * is_upper - Entry point
+/**
+ * isupper - Entry point
  * @c: stores the integer
  * Return: 0
  */
 
 int _isupper(int c)
 {
-	if(isupper(c))
+	if (isupper(c))
 	{
 		return (1);
 	}
