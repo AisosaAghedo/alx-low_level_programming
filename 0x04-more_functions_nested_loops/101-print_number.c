b92770613c1a16beeb09c4042ca7de27ba5ab2db
Aisosa <allysonaghedo@gmail.com>
2022-04-15 07:39:13 -0700

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,6 +7,19 @@
  */
 void print_number(int n)
 {
-	_putchar('n');
+	unsigned int m;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		m = -n;
+	}
+	else
+	{
+		m = n;
+	}
+	if (m / 10 != 0)
+		print_number(m / 10);
+	_putchar((m % 10) + '0');
 }
 
