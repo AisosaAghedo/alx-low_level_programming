8b44490277ac93b1e97207f99297fb69e5c00036
Aisosa <allysonaghedo@gmail.com>
2022-04-20 11:52:18 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - function to change value
+ * @n: integer that receives address
+ * Return: 0 
+ */
+void reset_to_98(int *n)
+{
+	*n = 98
+}
