5e243f62e88319d54e9bd9598624f956cfdaa7b1
Aisosa <allysonaghedo@gmail.com>
2022-04-20 12:23:09 -0700

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strlen - function to print the length of a string
+ * @s: string to check the length
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	printf("%d", strlen(s));
+}
