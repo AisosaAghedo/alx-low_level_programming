3ce90706561e63c3fa156890b9675c30dbc45d7d
Aisosa <allysonaghedo@gmail.com>
2022-04-20 13:36:27 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - funtion to print out a string
+ * @str: string to be printed out
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	printf("%c\n", str);
+}
