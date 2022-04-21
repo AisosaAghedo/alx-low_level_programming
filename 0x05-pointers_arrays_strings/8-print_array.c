3cdbf39d3e86f308cec2dad7a335327f072ba075
Aisosa <allysonaghedo@gmail.com>
2022-04-20 17:22:07 -0700

    array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,11 +13,15 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
+
+	if (i == (n - 1))
+	{
 		printf("%d", a[i]);
 	}
-	if ((i < n) - 1) 
+	else
 	{
-		printf(", ");
+		printf("%d, ", a[i]);
+	}
 	}
 	printf("\n");
 }
