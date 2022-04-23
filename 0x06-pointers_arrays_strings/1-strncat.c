e0fb99bcbcd9cd5d4cf98c385ed63a9f7c0f30b0
Aisosa <allysonaghedo@gmail.com>
2022-04-23 03:25:07 -0700

    string n cat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _strncat - function that concatenates two strings
+ * @dest: first string
+ * @src: second string
+ * @n:  bytes from src
+ * Return: Alway 0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	return (strncat(dest, src, n));
+}
+		
