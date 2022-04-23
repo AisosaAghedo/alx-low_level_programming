b1475915a4779337bcf75d7a3708c2cedee57ba2
Aisosa <allysonaghedo@gmail.com>
2022-04-23 07:29:44 -0700

    to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * *string_toupper - function that changes all lowercase letters to uppercase
+ * @str: the string
+ * Return: Always 0
+ */
+char *string_toupper(char *str)
+{
+	return (toupper(str));
+}
