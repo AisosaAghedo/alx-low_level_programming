a6a2f662dddedf59cfe6869d1e8b6393d429c56b
Aisosa <allysonaghedo@gmail.com>
2022-04-26 14:07:07 -0700

    chessboards

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,18 +8,17 @@
  */
  void print_chessboard(char (*a)[8])
 {
-	int len;
 	int i;
 	int j;
 
 
-	len = strlen(a) - 1;
-	for (i =0; i <  len; i++)
+	for (i =0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
-			_putchar((a[i][j]));
+			_putchar (a[i][j]);
 		}
+		_putchar('\n');
 	}
 	return (0);
 }
