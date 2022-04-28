6b53ab54b2bf0ef53f744d2496a2fa12ad761740
Aisosa <allysonaghedo@gmail.com>
2022-04-28 15:36:37 -0700

    reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,15 +7,15 @@
 void _print_rev_recursion(char *s)
 {
 
-	if (*s)
+	if (*s == '\0')
 	{
 		return;
 	}
 	else
 	{
-
-		_print_rev_recursion(s + 1);
-
+		s++;
+		_print_rev_recursion(s);
+		s--;
 		_putchar(*s);
 
 	}
