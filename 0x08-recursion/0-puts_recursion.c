53958445bee4a1ae01759a41c1e7bf3d77197637
Aisosa <allysonaghedo@gmail.com>
2022-04-28 08:48:48 -0700

    puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ * @s: variable that holds string
+ * Return: string followed by a new line
+ */
+void _puts_recursion(char *s)
+{
+	return (puts(s));
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Recursion is calling a function within that same function
