a10353e1811bcb79d0486d8bb3b98ec6196c4013
Aisosa <allysonaghedo@gmail.com>
2022-04-28 10:48:33 -0700

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <libm/math.h>
+#include <math.h>
 /**
  *  _pow_recursion(int x, int y) - function that returns the value of x raised to the power of y
  *  @x: base value
