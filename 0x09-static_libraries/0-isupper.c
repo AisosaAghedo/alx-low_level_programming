bfeaa8ad5c22e2a07102a4afca14f98ab1840cfb
Aisosa <allysonaghedo@gmail.com>
2022-05-02 15:14:52 -0700

    v

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,4 +1,39 @@
 #include "main.h"
+
+#include <stdio.h>
+
+#include <ctype.h>
+
+/**
+ *
+ *  *_isupper - Function to check for uppercases
+ *
+ *   * @c: variable to store character
+ *
+ *    * Return: 0
+ *
+ *     */
+
+
+
 int _isupper(int c)
+
 {
+
+		if (isupper(c))
+
+				{
+
+							return (1);
+
+								}
+
+			else
+
+					{
+
+								return (0);
+
+									}
+
 }
