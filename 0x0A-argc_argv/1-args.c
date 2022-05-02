bf76879396401557582a0b4aaba5d98f4d34dc5b
Aisosa <allysonaghedo@gmail.com>
2022-05-02 05:56:25 -0700

    argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argv: argument vector
+ * @argc: number of strings
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
