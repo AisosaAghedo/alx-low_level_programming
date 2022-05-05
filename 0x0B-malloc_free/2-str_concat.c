8cc3862220657039d26ea91c43390a0cf6ea6368
Aisosa <allysonaghedo@gmail.com>
2022-05-05 12:41:13 -0700

    string cat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * *str_concat -  function that concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: value
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *r;
+
+	if (s1 = NULL || s2 = NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		r = strcat(s1, s2);
+	}
+	return (r);
