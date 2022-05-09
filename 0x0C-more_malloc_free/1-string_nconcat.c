5e0a14b860c8421366d28dbc01d74fdfa93c553e
Aisosa <allysonaghedo@gmail.com>
2022-05-09 08:44:52 -0700

    strncat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s2 = "";
 	}
 	l = str1 + str2 + 1;
-	p = (char *) malloc(sizeof(char) * l);
+	p = malloc(l);
 	if (p == NULL)
 	{
 		return (NULL);
