07cf3f156012fe5fd0e326fada74319c8324dee6
Aisosa <allysonaghedo@gmail.com>
2022-05-12 07:55:13 +0000

    nil

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,4 +11,25 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
+	else if (d->name == NULL)
+	{
+		printf(Name: (nil));
+	}
+	else if (d->age == NULL)
+	{
+		printf(Age: (nil));
+	}
+	else if (d->owner == NULL)
+	{
+		printf(Owner: (nil));
+	}
 	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
+
+
+				
