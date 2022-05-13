a08a5e408716cd9903bd42329bb14bf956ed566c
Aisosa <allysonaghedo@gmail.com>
2022-05-13 23:11:56 +0100

    array interator

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * int_index -  function that searches for an integer
+ * @array: array of numbers
+ * @size:  number of elements in the array array
+ * @cmp: pointer to the function to be used to compare values
+ * Return: value
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	if (cmp == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		cmp(array[i]);
+	}
+	return (-1);
+}
+
