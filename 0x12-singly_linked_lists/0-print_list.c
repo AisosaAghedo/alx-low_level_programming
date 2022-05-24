aef522965b32d55f142ab3fe9412ff572ce29709
Aisosa <allysonaghedo@gmail.com>
2022-05-24 21:40:59 +0100

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,7 +5,7 @@
 /**
  * print_list - function that prints all the elements of a list_t list
  * @h: pointer
- * Return number of nodes
+ * Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
