483a71fbd30f2ffb27acc117517a6a8b97e5cce5
Aisosa <allysonaghedo@gmail.com>
2022-05-24 21:53:59 +0100

    list length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * list_len - function that returns the number of elements
+ * @h: pointer
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *pt;
+	int i;
+
+	pt = h;
+	for (i = 0; tmp != NULL; i++)
+	{
+		pt = pt->next;
+	}
+	return (i);
+}
