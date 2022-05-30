069bf54f0a86df4cd217fe7b16d8dcf0271da393
Aisosa <allysonaghedo@gmail.com>
2022-05-30 22:02:20 +0100

    index value

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,7 +11,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int indexVal;
 
-	if (index  > (sizeof(unsigned long int ) * 8 - 1))
+	if (index  > (sizeof(unsigned long int) * 8 - 1))
 	{
 		return (-1);
 	}
