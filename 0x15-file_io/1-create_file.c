62389288ebaf2c5dc281f1aad3202881d9b3e3ce
Aisosa <allysonaghedo@gmail.com>
2022-06-01 22:00:01 +0100

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "main.h"
+/**
+ * create_file -  function that creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, rw);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	close(fd);
+	return(1);
+}
+
