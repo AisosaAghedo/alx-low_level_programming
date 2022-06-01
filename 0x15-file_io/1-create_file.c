922fbe666316e93ea70961025949cf8e016a6282
Aisosa <allysonaghedo@gmail.com>
2022-06-01 22:43:04 +0100

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, wr;
+	int fd, wr, i;
 
 	if (filename == NULL)
 	{
@@ -27,8 +27,11 @@ int create_file(const char *filename, char *text_content)
 	
 	if (text_content == NULL)
 	{
-		wr = write(fd, "", 1);
-		if (wr == -1)
+		 for (i = 0; text_content[i]; i++)
+            ;
+        wr = write(fd, text_content, i);
+		
+	if (wr == -1)
 		{
 			return (-1);
 		}
