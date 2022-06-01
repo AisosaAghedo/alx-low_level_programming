3157563e463493249dba59612dd53a8066112712
Aisosa <allysonaghedo@gmail.com>
2022-06-01 23:13:56 +0100

    append a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,12 +16,10 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 
-	 if (filename == NULL)
-        {
-                return (-1);
-        }
-
-
+	if (filename == NULL)
+	{
+		return (-1);
+	}
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 	{
