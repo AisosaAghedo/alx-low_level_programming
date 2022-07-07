5ddb0e002a3262750f0c7b893fdf92e7d2558296
Aisosa <allysonaghedo@gmail.com>
2022-07-07 20:55:02 +0100

    number of elements in a double linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - function that returns the number of elements in a doubly linked list
+ * @h: pointer to first node of the doubly linked list
+ * Return: number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *temp = h;
+	size_t length = 0;
+
+	while (temp != NULL)
+	{
+		length++;
+		temp = temp->next;
+	}
+	return (length);
+}
