efb87c7c715586f69f2529a0eee0da8d60e4b408
Aisosa <allysonaghedo@gmail.com>
2022-09-14 22:46:18 +0100

    technical question

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -14,21 +14,15 @@ The island doesn’t have “lakes” (water inside that isn’t connected to th
 
 
 def island_perimeter(grid):
-    """
-    returns the perimeter of the island described in 'grid'
-    """
-    width = len(grid[0])
-    height = len(grid)
-    size = 0
-
-    for i in range(height):
-        for j in range(width):
-            if grid[i][j] == 1:
-                size += 1
-            if i != 0:
-                if grid[i - 1][j] == 1 and grid[i][j + 1] == 1:
-                    size += 1
-            if i != height - 1:
-                if grid[i + 1][j] == 1 and grid[i][j + 1] == 1:
-                    size += 1
-    return size * 2
+    rows = len(grid)
+    perimeter = 0
+    for i in range(rows):
+        cols = len(grid[i])
+        for j in range(cols):
+            if (grid[i][j] == 1):
+                left = 1 if j == 0 or grid[i][j - 1] == 0 else 0
+                top = 1 if i == 0 or grid[i - 1][j] == 0 else 0
+                right = 1 if j == cols - 1 or grid[i][j + 1] == 0 else 0
+                bottom = 1 if i == rows - 1 or grid[i + 1][j] == 0 else 0
+                perimeter += (top + right + bottom + left)
+    return perimeter
